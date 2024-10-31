6d1eef69a978a2433d07b3b7a4e6011f824bf52f
samefede <federico.farinia@gmail.com>
2024-10-31 20:39:18 -0300

    Config Typo y Color

--- app/src/main/java/com/example/parcialtp3/ui/theme/Color.kt
+++ app/src/main/java/com/example/parcialtp3/ui/theme/Color.kt
@@ -9,3 +9,29 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF2A1846)
+val White = Color(0xFF0FD08B)
+
+val Gray900 = Color(0xFF575372)
+val Gray500 = Color(0xFFD1D1D7)
+val Gray300 = Color(0xFFEEEEF0)
+val Gray100 = Color(0xFFF8F8F6)
+
+val Green900 = Color(0xFF00A067)
+val Green800 = Color(0xFF0FD08B)
+val Green100 = Color(0xFFCFF6E8)
+
+val Blue900 = Color(0xFF478FF2)
+val Blue500 = Color(0xFF478FF2)
+val Blue100 = Color(0xFFDAE9FC)
+
+val Purple900 = Color(0xFF442E83)
+
+val Red900 = Color(0xFFF96440)
+val Red100 = Color(0xFFFEE0D9)
+
+val Yellow900 = Color(0xFFFFE05F)
+val Yellow800 = Color(0xFFFFE05F)
+val Yellow500 = Color(0xFFFFEC9F)
+val Yellow100 = Color(0xFFFFF9DF)
