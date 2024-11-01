01d95a61364ff18b9c87417385bfbbc924fa978f
agus <agusalta002@gmail.com>
2024-11-01 17:00:27 -0300

    Update RadioButton

--- app/src/main/java/com/example/parcialtp3/components/Check.kt
+++ app/src/main/java/com/example/parcialtp3/components/Check.kt
@@ -3,6 +3,7 @@ package com.example.parcialtp3.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -19,39 +20,56 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.parcialtp3.R
 import com.example.parcialtp3.ui.theme.Black
-import com.example.parcialtp3.ui.theme.Green800
 import com.example.parcialtp3.ui.theme.TextXS1Regular
 import com.example.parcialtp3.ui.theme.White2
 
 @Composable
 fun CustomRadioButton(text: String) {
     var checkedState by remember { mutableStateOf(false) }
+    var isFocused by remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .background(White2)
-            .padding(horizontal = 16.dp, vertical = 8.dp),
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .focusable()
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            }
+            .clickable {
+                isFocused = true
+            }
+        ,
         horizontalArrangement = Arrangement.Absolute.Left,
         verticalAlignment = Alignment.CenterVertically
+
     ) {
         Box(
             modifier = Modifier
                 .clip(CircleShape)
-                .background(if (checkedState) Green800 else White2)
+                .clickable {
+                    checkedState = !checkedState
+                    isFocused = false
+                }
+                .padding(4.dp)
         ) {
-            val icon = if (checkedState) R.drawable.radio_button_focus else R.drawable.radio_button_inactive
+            val icon = when {
+                checkedState -> R.drawable.radio_button_success
+                isFocused -> R.drawable.radio_button_focus
+                else -> R.drawable.radio_button_inactive
+            }
             Image(
                 painter = painterResource(id = icon),
                 contentDescription = if (checkedState) "Radio Button Active" else "Radio Button Inactive",
                 modifier = Modifier
                     .size(30.dp)
-                    .clickable { checkedState = !checkedState }
             )
         }
 
@@ -61,7 +79,6 @@ fun CustomRadioButton(text: String) {
             color = Black,
             modifier = Modifier
                 .padding(start = 8.dp)
-
         )
     }
 }
