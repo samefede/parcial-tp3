4c4b1264def13a88eafb2deacc44c590c3e25da6
samefede <federico.farinia@gmail.com>
2024-11-02 20:17:03 -0300

    HomeScreen LazyColumn

--- app/src/main/java/com/example/parcialtp3/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/parcialtp3/screens/home/HomeScreen.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -42,66 +46,70 @@ fun HomeScreen(
 
     val walletState by walletViewModel.wallet.observeAsState()
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(Gray100)
             .padding(12.dp)
     ) {
-        Text(
-            text = "\uD83D\uDC4B Hola Mariana",
-            style = TextXL1Bold,
-            color = Black
-        )
+        item {
+            Card("4957 1234 12345 5824")
 
-        Text(
-            text = "Ultimo acceso: Mar 01, 2020 4:55 PM",
-            style = TextXS2Regular,
-            color = Black
-        )
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
 
-        Spacer(
-            modifier = Modifier.height(26.dp)
-        )
-
-        Card("4957 1234 12345 5824")
-
-        Spacer(
-            modifier = Modifier.height(24.dp)
-        )
-
-        walletState?.get(0)?.let {
-            TotalAmount(amount = it.balance, textSize = TextSize.BIG)
-        }
-
-        Spacer(
-            modifier = Modifier.height(24.dp)
-        )
-
-        AlertCustom(
-            alertText = "La cuota de tu préstamo está próxima a vencer.",
-            linkText = "Ver préstamo"
-        )
-
-        Spacer(
-            modifier = Modifier.height(24.dp)
-        )
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth().padding(12.dp)
-        ) {
-            Row(){
-                ActionCard(iconResId = R.drawable.cargardinero, textLine1 = "Cargar", textLine2 = "Dinero", topLeftCornerRadius = 8.dp)
-                ActionCard(iconResId = R.drawable.extraerdinero, textLine1 = "Extraer", textLine2 = "Dinero")
-                ActionCard(iconResId = R.drawable.prestamos, textLine1 = "Seguir", textLine2 = "Mi Préstamo", topRightCornerRadius = 8.dp)
+            walletState?.get(0)?.let {
+                TotalAmount(amount = it.balance, textSize = TextSize.BIG)
             }
-            Row() {
-                ActionCard(iconResId = R.drawable.recargasube, textLine1 = "Recarga", textLine2 = "Sube", bottomLeftCornerRadius = 8.dp)
-                ActionCard(iconResId = R.drawable.recargacelu, textLine1 = "Recarga", textLine2 = "Celular")
-                ActionCard(iconResId = R.drawable.pagoservicio, textLine1 = "Pagar", textLine2 = "Servicio", bottomRightCornerRadius = 8.dp)
+
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+
+            AlertCustom(
+                alertText = "La cuota de tu préstamo está próxima a vencer.",
+                linkText = "Ver préstamo"
+            )
+
+            Spacer(
+                modifier = Modifier.height(24.dp)
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ) {
+                Row(modifier = Modifier.fillMaxWidth().height(96.dp)) {
+                    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                        item {
+                            ActionCard(iconResId = R.drawable.cargardinero, textLine1 = "Cargar", textLine2 = "Dinero", topLeftCornerRadius = 8.dp)
+                        }
+                        item {
+                            ActionCard(iconResId = R.drawable.extraerdinero, textLine1 = "Extraer", textLine2 = "Dinero")
+                        }
+                        item {
+                            ActionCard(iconResId = R.drawable.prestamos, textLine1 = "Seguir", textLine2 = "Mi Préstamo", topRightCornerRadius = 8.dp)
+                        }
+                    }
+                }
+
+                Row(modifier = Modifier.fillMaxWidth().height(96.dp)){
+                    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                        item {
+                            ActionCard(iconResId = R.drawable.recargasube, textLine1 = "Recarga", textLine2 = "Sube", bottomLeftCornerRadius = 8.dp)
+                        }
+                        item {
+                            ActionCard(iconResId = R.drawable.recargacelu, textLine1 = "Recarga", textLine2 = "Celular")
+                        }
+                        item {
+                            ActionCard(iconResId = R.drawable.pagoservicio, textLine1 = "Pagar", textLine2 = "Servicio", bottomRightCornerRadius = 8.dp)
+                        }
+                    }
+                }
             }
         }
-
     }
 }
