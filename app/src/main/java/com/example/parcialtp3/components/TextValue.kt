f425f31d005af4a878d3a9ad1309a293c9d59364
agus <agusalta002@gmail.com>
2024-11-02 10:35:06 -0300

    Add Card de Saldo disponible

--- app/src/main/java/com/example/parcialtp3/components/TextValue.kt
+++ app/src/main/java/com/example/parcialtp3/components/TextValue.kt
@@ -1,28 +1,31 @@
 package com.example.parcialtp3.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.parcialtp3.ui.theme.Black
 import com.example.parcialtp3.ui.theme.Gray900
 import com.example.parcialtp3.ui.theme.TextBaseBold
 import com.example.parcialtp3.ui.theme.TextBaseRegular
+import com.example.parcialtp3.ui.theme.White2
 
 @Composable
-fun TextValue(label: String, value: String) {
-    Row {
+fun TextValue( label: String, value: String) {
+    Row(modifier = Modifier.background(White2)) {
         Text(
             text = label,
             color = Gray900,
-            style = TextBaseRegular
+            style = TextBaseRegular,
         )
         Text(
             text = value,
             color = Black,
             style = TextBaseBold,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
         )
     }
 }
