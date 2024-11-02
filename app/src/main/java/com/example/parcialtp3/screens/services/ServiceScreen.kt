42c5afb7257e105a850ef4f9d9e97025f41154f8
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2024-11-02 03:29:54 -0300

    Service Screen - Acomodar al figma

--- app/src/main/java/com/example/parcialtp3/screens/services/ServiceScreen.kt
+++ app/src/main/java/com/example/parcialtp3/screens/services/ServiceScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.parcialtp3.navigation.MainNavAction
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.graphics.Color
-
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.ui.unit.dp
+import com.example.parcialtp3.R
+import com.example.parcialtp3.components.ServiceCard
+import com.example.parcialtp3.ui.theme.Gray100
 
 @Composable
 fun ServiceScreen(
@@ -17,8 +23,29 @@ fun ServiceScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Green)
+            .background(Gray100)
+            .padding(25.dp)
     ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.spacedBy(25.dp),
+            verticalArrangement = Arrangement.spacedBy(25.dp)
+        ) {
+            item {
+                ServiceCard(iconResId = R.drawable.recargasube, serviceText = "RECARGA SUBE")
+            }
+            item {
+                ServiceCard(iconResId = R.drawable.recargacelu, serviceText = "RECARGA CELULAR")
+            }
+            item {
+                ServiceCard(iconResId = R.drawable.pagoservicio, serviceText = "PAGO DE SERVICIOS")
+            }
+            item {
+                ServiceCard(iconResId = R.drawable.directtv, serviceText = "DIRECT TV PREPAGO")
+            }
+        }
 
     }
 }
