b96cf0f18bfb7e76c6370481eb1dc917e86e806d
samefede <federico.farinia@gmail.com>
2024-11-02 20:16:41 -0300

    Topbar component

--- app/src/main/java/com/example/parcialtp3/navigation/MainNavAction.kt
+++ app/src/main/java/com/example/parcialtp3/navigation/MainNavAction.kt
@@ -1,7 +1,13 @@
 package com.example.parcialtp3.navigation
 
 import android.widget.MediaController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.navigation.NavHostController
+import com.example.parcialtp3.ui.theme.Black
+import com.example.parcialtp3.ui.theme.Gray100
+import com.example.parcialtp3.ui.theme.TextXL1Bold
 
 object AppDestinations{
     const val SIGN_IN_ROUTE = "signin"
@@ -14,6 +20,11 @@ object AppDestinations{
 }
 
 val hideBottomBar= listOf(
+    AppDestinations.SIGN_IN_ROUTE,
+    AppDestinations.SPLASH_ROUTE,
+)
+
+val hideTopBar= listOf(
     AppDestinations.SIGN_IN_ROUTE,
     AppDestinations.SPLASH_ROUTE,
 )
@@ -48,5 +59,64 @@ class MainNavAction(
 
     fun hideBottomBar(location: String?): Boolean{
         return hideBottomBar.contains(location)
+    }
+
+    fun hideTopBar(location: String?): Boolean{
+        return hideTopBar.contains(location)
+    }
+
+    fun getColorTopBar(location: String?): Color{
+        return when(location){
+            AppDestinations.HOME_ROUTE -> Gray100
+            AppDestinations.ACCOUNT_ROUTE -> Gray100
+            AppDestinations.CREDIT_ROUTE -> Gray100
+            AppDestinations.PROFILE_ROUTE -> Gray100
+            AppDestinations.SERVICE_ROUTE -> Gray100
+            else -> Color.White
+        }
+    }
+
+    fun getTextTopBar(location: String?): String{
+        return when(location){
+            AppDestinations.HOME_ROUTE -> "\uD83D\uDC4B Hola Mariana"
+            AppDestinations.ACCOUNT_ROUTE -> "Mi Cuenta"
+            AppDestinations.CREDIT_ROUTE -> "Mi Tarjeta"
+            AppDestinations.PROFILE_ROUTE -> "Mi perfil"
+            AppDestinations.SERVICE_ROUTE -> "Pago de servicios"
+            else -> ""
+        }
+    }
+
+    fun getTitleStyleTopBar(location: String?): TextStyle{
+        return when(location){
+            AppDestinations.HOME_ROUTE -> TextXL1Bold
+            AppDestinations.ACCOUNT_ROUTE -> TextXL1Bold
+            AppDestinations.CREDIT_ROUTE -> TextXL1Bold
+            AppDestinations.PROFILE_ROUTE -> TextXL1Bold
+            AppDestinations.SERVICE_ROUTE -> TextXL1Bold
+            else -> TextXL1Bold
+        }
+    }
+
+    fun getTitleColorTopBar(location: String?): Color{
+        return when(location){
+            AppDestinations.HOME_ROUTE -> Black
+            AppDestinations.ACCOUNT_ROUTE -> Black
+            AppDestinations.CREDIT_ROUTE -> Black
+            AppDestinations.PROFILE_ROUTE -> Black
+            AppDestinations.SERVICE_ROUTE -> Black
+            else -> Black
+        }
+    }
+
+    fun getDescriptionTopBar(location: String?): String{
+        return when(location){
+            AppDestinations.HOME_ROUTE -> "Ultimo acceso: Mar 01, 2020 4:55 PM"
+            AppDestinations.ACCOUNT_ROUTE -> ""
+            AppDestinations.CREDIT_ROUTE -> ""
+            AppDestinations.PROFILE_ROUTE -> ""
+            AppDestinations.SERVICE_ROUTE -> ""
+            else -> ""
+        }
     }
 }
